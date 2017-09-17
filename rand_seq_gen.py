809751b7db8666aad29333d3385049eca94b7c19
Grzegorz Maciag <grzegorz.maciag@hotmail.com>
2017-09-17 12:20:28 +0200

    For use on binf server

--- rand_seq_gen.py
+++ rand_seq_gen.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 16 21:00:50 2017
+Created on Sun Sep 17 10:56:28 2017
 
-@author: xvk205
+Script for generating a single random sequence of length 200.
+To be run 100 times for the exercise. 
+
+@author: GMA
 """
-from random import choices
+
+from numpy import array
+# Older version of python on binf server prevents using choices module. 
+from numpy.random import choice
 from textwrap import fill
 
-DB_SIZE = 4183
-DB_LENGTH = 319
-SEQ_SIZE = 100
 SEQ_LENGTH = 200
 
 amino_acids = "ACDEFGHIKLMNPQRSTVWY"
@@ -18,21 +21,16 @@ aa_list = list(amino_acids)
 frequences = [0.095070, 0.011560, 0.051634, 0.057745, 0.039081, 
               0.073548, 0.022629, 0.060123, 0.043981, 0.106769, 
               0.028008, 0.039309, 0.044342, 0.044409, 0.055342, 
-              0.058097, 0.053810, 0.070647, 0.015378, 0.028498]
-
-def make_database():
-    with open("database.fasta","w") as db:
-        for i in range(DB_SIZE):
-            db.write(">DB_{}\n".format(str(i+1)))
-            db.write("{}\n".format(fill(''.join(
-                    choices(aa_list, frequences, k=DB_LENGTH)), 71)))
-            
+              0.058097, 0.053810, 0.070647, 0.015378, 0.028498]            
+# Frequencies (probabilities) have to be normalized for numpy.
+frequences = array(frequences)
+frequences /= frequences.sum()
+              
 def make_sequence():
+    # Each run will overwrite the file, supplying water with new seq. 
     with open("sequence.fasta","w") as seq:
-        for i in range(SEQ_SIZE):
-            seq.write(">SEQ_{}\n".format(str(i+1)))
-            seq.write("{}\n".format(fill(''.join(
-                    choices(aa_list, frequences, k=SEQ_LENGTH)), 71)))
+        seq.write(">SEQ\n")
+        seq.write("{}\n".format(fill(''.join(
+                    choice(aa_list, SEQ_LENGTH, p=frequences)), 71)))
             
-make_database()
 make_sequence()
